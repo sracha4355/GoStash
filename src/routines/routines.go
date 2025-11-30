fc0dbff77b94abcd1ae7f82edba152fc9e59122b
Satvik Racharla <satvik.racharla@us.mcd.com>
2025-11-30 16:17:06 -0500

    redid the api, but still need to update and move the testing to confirm its working

--- src/routines/routines.go
+++ src/routines/routines.go
@@ -13,6 +13,7 @@ import (
 	"github.com/sracha4355/psp/src/utils"
 )
 
+// ---- New API in progress ---- will need to refactor tests
 type AsyncFileWriterConfig struct {
 	WhenToFlush   int
 	FlushInterval time.Duration
@@ -23,7 +24,7 @@ type AsyncFileWriter[T contracts.Serializable[T]] struct {
 	wg           *sync.WaitGroup
 	InputChannel <-chan T
 	ErrorChannel chan<- error
-	DoneChannel  chan<- struct{}
+	DoneChannel  <-chan struct{}
 	Ctx          context.Context
 	Config       AsyncFileWriterConfig
 }
@@ -35,7 +36,7 @@ func (w *AsyncFileWriter[T]) NewAsyncFileWriter(
 	__writer__ *io.Writer,
 	__input_channel__ <-chan T,
 	__error_channel__ chan<- error,
-	__done_channel__ chan<- struct{},
+	__done_channel__ <-chan struct{},
 	__ctx__ context.Context,
 	__waitgroup__ *sync.WaitGroup,
 ) {
@@ -52,10 +53,118 @@ func (w *AsyncFileWriter[T]) NewAsyncFileWriter(
 	}
 }
 
-func (w *AsyncFileWriter[T]) Run() {
-	
+func (afw *AsyncFileWriter[T]) Run() {
+	defer func() {
+		afw.wg.Done()
+	}()
+	var internals *AsyncFileWriterConfig = &afw.Config
+	if internals.WhenToFlush <= 0 {
+		internals.WhenToFlush = 100
+	}
+
+	// -- pass value for buz size later
+	w := bufio.NewWriter(*internals.Writer)
+	ticker := time.NewTicker(internals.FlushInterval)
+	defer func() {
+		_ = w.Flush()
+	}()
+
+	for {
+		select {
+		case <-afw.DoneChannel:
+			if errorWhileDraining := afw.__drain__(w); errorWhileDraining != nil {
+				utils.TrySendError(afw.ErrorChannel, errorWhileDraining, internals.SupressLogs)
+			}
+			if errorWhileFlushing := afw.__flush__(w); errorWhileFlushing != nil {
+				utils.TrySendError(afw.ErrorChannel, errorWhileFlushing, internals.SupressLogs)
+			}
+			goto done
+		case <-afw.Ctx.Done():
+			if !internals.SupressLogs {
+				utils.LogWithContext(utils.Info{}, "Context cancellation occurred:%v", afw.Ctx.Err())
+			}
+			goto done
+		case item, ok := <-afw.InputChannel:
+			if !ok {
+				if !internals.SupressLogs {
+					utils.LogWithContext(utils.Info{}, "Input channel closed --- exiting")
+				}
+				if errorWhileFlushing := afw.__flush__(w); errorWhileFlushing != nil {
+					utils.TrySendError(afw.ErrorChannel, errorWhileFlushing, internals.SupressLogs)
+				}
+				goto done
+			}
+			if !internals.SupressLogs {
+				utils.LogWithContext(utils.Info{}, "Received input (len=%d)", w.Size())
+			}
+			var errorWhileWriting error = nil
+			if errorWhileWriting := afw.__write__(item, w); errorWhileWriting != nil {
+				utils.TrySendError(afw.ErrorChannel, errorWhileWriting, internals.SupressLogs)
+			}
+			// ---- Flush on failure
+			if errorWhileWriting != nil {
+				if errorWhileFlushing := afw.__flush__(w); errorWhileFlushing != nil {
+					utils.TrySendError(afw.ErrorChannel, errorWhileFlushing, internals.SupressLogs)
+				}
+				goto done
+			}
+		case <-ticker.C:
+			if !internals.SupressLogs {
+				utils.LogWithContext(utils.Info{}, "Ticker fired, flushing %d inputs", w.Size())
+			}
+			if errorWhileFlushing := afw.__flush__(w); errorWhileFlushing != nil {
+				utils.TrySendError(afw.ErrorChannel, errorWhileFlushing, internals.SupressLogs)
+				goto done
+			}
+		}
+	}
+done:
+	return
 }
 
+/**
+* Primary assumption behind @__drain__ is that no other routines are writing to afw.InputChannel
+* Will bubble up errors and @Run() will write to afw.ErrorChannel
+ */
+func (afw *AsyncFileWriter[T]) __drain__(
+	w *bufio.Writer,
+) error {
+	if len(afw.InputChannel) != 0 {
+		for item := range afw.InputChannel {
+			if err := afw.__write__(item, w); err != nil {
+				return err
+			}
+			if len(afw.InputChannel) == 0 {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (afw *AsyncFileWriter[T]) __write__(
+	input T,
+	w *bufio.Writer,
+) error {
+	if _, err := w.Write(input.Serialize()); err != nil {
+		return fmt.Errorf("Write error: %w", err)
+	}
+	return nil
+}
+
+func (afw *AsyncFileWriter[T]) __flush__(
+	w *bufio.Writer,
+) error {
+	if err := w.Flush(); err != nil {
+		return fmt.Errorf("Failed to flush: %w", err)
+	}
+	if !afw.Config.SupressLogs {
+		utils.LogWithContext(utils.Info{}, "Successfully flushed %d bytes", w.Buffered())
+	}
+	return nil
+}
+
+// ---- Old API
 func TrySendError(__errChan__ chan<- error, __err__ error, doLog bool) {
 	select {
 	case __errChan__ <- __err__:
@@ -78,10 +187,8 @@ func flushAndSync(w *bufio.Writer, f *os.File) error {
 
 // ---- WARNING readAndFlushLinks's intended behavior is to empty links array after processing
 func readAndFlushLinks(
-	links *[]string,
 	w *bufio.Writer,
 	f *os.File,
-	supressLogs bool,
 ) error {
 	if !supressLogs {
 		utils.LogWithContext(utils.Info{}, "%d links to flush", len(*links))
